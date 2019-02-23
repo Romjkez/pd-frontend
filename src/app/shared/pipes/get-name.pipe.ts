21bddc9a2292231c52ac51368ba15a56d5a4487a
Romka023 <meshkov.ra@yandex.ru>
2019-02-23 18:08:10 +0300

    Display team members done(group numbers are static)

--- src/app/shared/pipes/get-name.pipe.ts
+++ src/app/shared/pipes/get-name.pipe.ts
@@ -1,12 +1,21 @@
 import {Pipe, PipeTransform} from '@angular/core';
+import {ApiService} from '../services/api.service';
+import {from, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Pipe({
   name: 'getName'
 })
 export class GetNamePipe implements PipeTransform {
-
-  transform(value: any, args?: any): any {
-    return null;
+  constructor(private apiService: ApiService) {
   }
 
+  transform(value: number): Observable<any> {
+    return from(this.apiService.getUserById(value))
+      .pipe(
+        map(({name, surname, middle_name}) => {
+          return `${surname} ${name.length > 0 ? name : ''} ${middle_name.length > 0 ? middle_name : ''}`;
+        }),
+      );
+  }
 }
