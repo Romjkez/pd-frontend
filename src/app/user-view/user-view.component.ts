e81638afbb737f709ba00e18e9a40c480aba6636
Romka023 <meshkov.ra@yandex.ru>
2019-03-27 01:07:16 +0300

    Show user projects in profile

--- src/app/user-view/user-view.component.ts
+++ src/app/user-view/user-view.component.ts
@@ -2,7 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ApiService, User} from '../shared/services/api.service';
 import {AuthService, parseJwt} from '../shared/services/auth.service';
+import {Project} from '../shared/components/project-snippet/project-snippet.component';
 
+interface UserProjects {
+  active_projects: Project[];
+  finished_projects: Project[];
+}
 @Component({
   selector: 'app-user-view',
   templateUrl: './user-view.component.html',
@@ -12,6 +17,7 @@ export class UserViewComponent implements OnInit {
   user: User;
   canEdit: boolean;
   loading: boolean;
+  projects: UserProjects;
 
   constructor(private activatedRoute: ActivatedRoute, private apiService: ApiService, private router: Router,
               public authService: AuthService) {
@@ -26,18 +32,18 @@ export class UserViewComponent implements OnInit {
         this.user = res;
         if (this.authService.getToken().length > 0) {
           this.canEdit = parseJwt(this.authService.getToken()).data.id === res.id;
-
         } else {
           this.canEdit = false;
         }
-        this.loading = false;
       } else {
         this.router.navigate(['/404']);
       }
+    }).then(() => {
+      this.apiService.getUserProjects(this.user.id).then(res => {
+        this.projects = res;
+      });
+      this.loading = false;
     }).catch(e => console.error(e));
   }
 
-  switchPage(ev) {
-
-  }
 }
