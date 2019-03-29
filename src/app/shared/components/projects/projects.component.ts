b00eb39086abcff0b109d705f1b095dc23bd163e
Romjkez <meshkov.ra@ya.ru>
2019-03-29 12:35:24 +0300

    Generate projects components[include]

---
+++ src/app/shared/components/projects/projects.component.ts
@@ -0,0 +1,23 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Projects} from '../../services/api.service';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css']
+})
+export class ProjectsComponent implements OnInit {
+  @Input() projects: Projects[];
+  perPage = 5;
+  currentPage = 1;
+  totalPages: number;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  switchPage() {
+  }
+}
