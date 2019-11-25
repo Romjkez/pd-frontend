e4c0344fec20be017cdc93667381d29eec20a3bd
Romka023 <meshkov.ra@yandex.ru>
2019-11-25 20:43:23 +0300

    Minor server requests changes

--- src/app/modules/shared/services/chat.service.ts
+++ src/app/modules/shared/services/chat.service.ts
@@ -21,19 +21,19 @@ export class ChatService {
   addMessage(project_id: number, message: string): Observable<ChatMessage> {
     const data = `project_id=${project_id}&message=${message}`;
     const headers = new HttpHeaders().append('Content-Type', 'application/x-www-form-urlencoded');
-    return this.http.post<ChatMessage>(`${this.baseUrl}/chat/`, data, {observe: 'body', headers: headers});
+    return this.http.post<ChatMessage>(`${this.baseUrl}/chat/index.php`, data, {observe: 'body', headers: headers});
   }
 
   deleteMessage(messageId: number): Observable<ApiMessage> {
-    return this.http.delete<{ message: string }>(`${this.baseUrl}/chat/?message_id=${messageId}`, {observe: 'body'});
+    return this.http.delete<{ message: string }>(`${this.baseUrl}/chat/index.php?message_id=${messageId}`, {observe: 'body'});
   }
 
   editMessage(messageId: number, message: string): Observable<ApiMessage> {
-    return this.http.put<{ message: string }>(`${this.baseUrl}/chat/?message_id=${messageId}&message=${message}`, '',
+    return this.http.put<{ message: string }>(`${this.baseUrl}/chat/index.php?message_id=${messageId}&message=${message}`, '',
       {observe: 'body'});
   }
 
   getProjectMessages(project_id: number): Observable<ChatMessage[]> {
-    return this.http.get<ChatMessage[]>(`${this.baseUrl}/chat/?project_id=${project_id}`, {observe: 'body'});
+    return this.http.get<ChatMessage[]>(`${this.baseUrl}/chat/index.php?project_id=${project_id}`, {observe: 'body'});
   }
 }
